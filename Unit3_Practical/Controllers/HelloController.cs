b83a8d830666802f489dd4d79f66eb7cee6b2d65
Aayush Neupane <pfpaayups@gmail.com>
2025-09-23 19:25:38 +0545

    Added Unit3 practical along with HelloController to test http resquest and response

---
+++ Unit3_Practical/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unit3_Practical.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /Hello/Welcome
+        public IActionResult Welcome()
+        {
+            return Content("Welcome to ASP.NET Core! This is a response to your HTTP GET request.");
+        }
+
+        // GET: /Hello/Greet
+        public IActionResult Greet(string name)
+        {
+            return Content($"Hello, {name}! This response is dynamically generated.");
+        }
+    }
+}
---
+++ Unit3_Practical/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Unit3_Practical.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
