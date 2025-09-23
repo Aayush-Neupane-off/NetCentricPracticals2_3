e4acf6efd61efc5ff4d1b8c96d01597ec9160c5c
Aayush Neupane <pfpaayups@gmail.com>
2025-09-23 21:36:12 +0545

    Added Student's Controller,Model and Views

---
+++ Unit3_Practical/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unit3_Practical.Models;
+
+namespace Unit3_Practical.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Create a sample Student object
+            var student = new Student
+            {
+                Id = 1,
+                Name = "Aayush Neupane",
+                Age = 21,
+                Course = "BIT"
+            };
+
+            // Pass model to the view
+            return View(student);
+        }
+    }
+}
